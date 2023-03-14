b57bf2b0b5de526274c7e7ff2020305d2eaa0497
lindawendel <linda.m.wendel@gmail.com>
2023-03-14 23:47:29 +0100

    list-test

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ Vue.createApp({
     data() {
         return {
 
-            expenses: [
-                {id: 1, label: "10"},
-                {id: 2, label: "20"},
-                {id: 3, label: "30"}
-            ]
+            // expenses: [
+            //     {id: 1, label: "10"},
+            //     {id: 2, label: "20"},
+            //     {id: 3, label: "30"}
+            // ]
+
+            expenses: {
+              'expense-1':  {id: 1, label: "10"},
+              'expense-2': {id: 2, label: "20"},
+              'expense-3': {id: 3, label: "30"}
+            }
 
         }
     }
