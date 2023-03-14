585cad6b4528f29b973be61a53749b48559584cd
lindawendel <linda.m.wendel@gmail.com>
2023-03-15 00:20:02 +0100

    some code

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-Vue.createApp({
+const test = Vue.createApp({
     data() {
         return {
 
-            // expenses: [
-            //     {id: 1, label: "10"},
-            //     {id: 2, label: "20"},
-            //     {id: 3, label: "30"}
-            // ]
+            // Objects
+            expenses: [
+                {id: 1, label: 10},
+                {id: 2, label: 20},
+                {id: 3, label: 30}
+            ],
 
-            expenses: {
-              'expense-1':  {id: 1, label: "10"},
-              'expense-2': {id: 2, label: "20"},
-              'expense-3': {id: 3, label: "30"}
-            }
+            newExpense: '',
+
+            // newInputType: 'expense'
+
+            newInputType: 'expense'
+
+            // expenses: {
+            //   'expense-1':  {id: 1, label: "10"},
+            //   'expense-2': {id: 2, label: "20"},
+            //   'expense-3': {id: 3, label: "30"}
+            // }
 
         }
     }
